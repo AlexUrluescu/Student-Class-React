e1f717ddd9905e4ce8c8c9fd3b7c6f39072fbb14
Alexandre <alexurluescu23@gmail.com>
2023-03-02 21:58:03 +0200

    HeaderPage: Add text and color at the HeaderPage

--- src/components/HeaderPage.js
+++ src/components/HeaderPage.js
@@ -4,7 +4,10 @@ import "../assets/HeaderPage.css"
 const HeaderPage = () => {
     return(
         <div className="container">
-            <div className="title-container">Title</div>
+            <div className="title-container">
+                <h1>Computer Science Class</h1>
+                <h2>at University Lucian Blaga Sibiu</h2>
+            </div>
             <div className="photos-container">
                 <img src="/images/poza-Alex.jpg"/>
             </div>
