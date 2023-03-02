2e3cb3cd79191990ad6f43262646d7ad9e2bbbcf
Alexandre <alexurluescu23@gmail.com>
2023-03-02 21:44:38 +0200

    HeaderPage: Create the HeaderPage of Home
    This HeaderPage has a image inserted

---
+++ src/components/HeaderPage.js
@@ -0,0 +1,15 @@
+
+import "../assets/HeaderPage.css"
+
+const HeaderPage = () => {
+    return(
+        <div className="container">
+            <div className="title-container">Title</div>
+            <div className="photos-container">
+                <img src="/images/poza-Alex.jpg"/>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderPage;
