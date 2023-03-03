126b4bb0e395234d2de95e854efeb4c0cf85c393
Alexandre <alexurluescu23@gmail.com>
2023-03-03 09:43:27 +0200

    Add the method 'Add new students'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import {Routes, Route} from "react-router-dom"
 
+import { useState } from "react"
+
 // ---------- pages ---------------------------
 import Home from './pages/Home';
 import About from './pages/About';
@@ -14,8 +16,38 @@ import NavBar from './components/NavBar';
 import Table from './components/Table';
 import FormStudents from './components/FormStudents';
 
+const db = [
+  {
+    id: "100",
+    name: "Nistor",
+    last_name: "Andrei",
+    age: "21",
+    city: "Cluj"
+  },
+  {
+    id: "101",
+    name: "Nicolau",
+    last_name: "Paul",
+    age: "31",
+    city: "Bucuresti"
+  }
+]
+
 
 function App() {
+
+  const [students, setStudents] = useState(db)
+
+  const salutare = () => {
+    console.log("Salutare");
+  }
+
+  const addStudent = (student) => {
+    student.id = Date.now()
+    setStudents([...students, student])
+
+    console.log("Student adaugat");
+  }
 
   return (
     <div className="App">
@@ -23,8 +55,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>}>
-            <Route path="/about" element={<Table />}/>
-            <Route path="/about/edit" element={<FormStudents />}/>
+            <Route path="/about" element={<Table students={students} salutare={salutare}/>}/>
+            <Route path="/about/add" element={<FormStudents salutare={salutare} addStudent={addStudent}/>} />
+            <Route path="/about/edit/:id" element={<FormStudents salutare={salutare}/>}/>
         </Route>
       </Routes>
 
--- src/components/FormStudents.js
+++ src/components/FormStudents.js
@@ -1,25 +1,54 @@
 
 import { useNavigate } from "react-router-dom";
+import { useState } from "react"
 
-const FormStudents = () => {
+const initialForm = 
+    {
+        name: "",
+        last_name: "",
+        age: "",
+        city: ""
+    }
+
+
+const FormStudents = ({addStudent}) => {
 
     const navigate = useNavigate()
+
+    const [form, setForm] = useState(initialForm)
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         console.log("s-a trimis formularul");
+        console.log(form);
+
+        addStudent(form)
+        resetForm()
+
         navigate("/about")
+
+    }
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const resetForm = () => {
+        setForm(initialForm)
     }
 
     return(
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="name" placeholder="Name" />
-                <input type="text" name="last_name" placeholder="Last name" />
-                <input type="text" name="age" placeholder="Age" />
-                <input type="text" name="city" placeholder="City" />
+                <input onChange={handleChange} type="text" value={form.name} name="name" placeholder="Name" />
+                <input onChange={handleChange} type="text" value={form.last_name} name="last_name" placeholder="Last name" />
+                <input onChange={handleChange} type="text" value={form.age} name="age" placeholder="Age" />
+                <input onChange={handleChange} type="text" value={form.city} name="city" placeholder="City" />
 
                 <input type="submit" value="Send" />
 
